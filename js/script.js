869cd041550b326d70203973127e65e3d4a84886
Fernando2CA <fergodnan7.7.231@gmail.com>
2025-11-11 15:31:28 -0600

    init:Creando repo

---
+++ js/script.js
@@ -0,0 +1,34 @@
+const x = 10;
+console.log(x);
+
+// iteraciones 
+const cielo = document.getElementById("cielo");
+
+for(let i = 0; i < 5; i++){
+    cielo.innerHTML += '⭐';
+}
+
+const planetas =["🌍", "🪐", "🌕"]
+
+for(const planeta of planetas){
+    cielo.innerHTML += planeta;
+}
+
+let n = 0;
+while(n < 5){
+    cielo.innerHTML += "☁";
+    n++;
+
+}
+
+let m = 0;
+do{
+    cielo.innerHTML += "🚀"
+    m++;
+}while(m < 4)
+
+const btnEstretella = document.getElementById("btnEstrella");
+
+btnEstretella.addEventListener("click", ()=> {
+    cielo.innerHTML += "⭐";
+})
