e364240a41b9b0cc221ffb11b70d0f219c288b6e
Fernando2CA <fergodnan7.7.231@gmail.com>
2025-11-11 16:26:33 -0600

    ft:Se agrega ejemplo de eventos

--- js/script.js
+++ js/script.js
@@ -1,34 +1,53 @@
-const x = 10;
-console.log(x);
+// iteraciones
+const cielo=document.getElementById("cielo");
 
-// iteraciones 
-const cielo = document.getElementById("cielo");
-
-for(let i = 0; i < 5; i++){
-    cielo.innerHTML += '⭐';
+for(let i=0;i<5;i++){
+    cielo.innerHTML +='⭐';
 }
 
-const planetas =["🌍", "🪐", "🌕"]
+const planetas = ["🌍", "🪐", "🌕"]
 
 for(const planeta of planetas){
     cielo.innerHTML += planeta;
 }
 
-let n = 0;
-while(n < 5){
-    cielo.innerHTML += "☁";
-    n++;
-
+let n=0;
+while(n<5){
+    cielo.innerHTML += "☁️"
+    n++
 }
 
-let m = 0;
+let m=0;
 do{
     cielo.innerHTML += "🚀"
     m++;
-}while(m < 4)
+}while(m<4)
 
-const btnEstretella = document.getElementById("btnEstrella");
+// eventos
+//click
+const btnEstrella = document.getElementById("btnEstrella");
 
-btnEstretella.addEventListener("click", ()=> {
+btnEstrella.addEventListener("click", ()=> {
     cielo.innerHTML += "⭐";
+})
+
+//dobleclick
+const btnPlaneta= document.getElementById("btnPlaneta");
+
+btnPlaneta.addEventListener("dblclick",()=>{
+    cielo.innerHTML +=  "🪐";
+})
+
+const inputEmoji = document.getElementById("inputEmoji");
+
+const btnAgregarEmoji = document.getElementById("btnAgregarEmoji")
+
+btnAgregarEmoji.addEventListener("click", ()=>{
+    var emoji = inputEmoji.value.trim();
+    if(emoji) cielo.innerHTML += emoji;
+    inputEmoji.value="";
+})
+
+inputEmoji.addEventListener("keydown", e =>{
+    if(e.key==="Escape") cielo.innerHTML="";
 })
